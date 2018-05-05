83dfeed17d4081778a3b81ae6afdf5ceade6c3a2
vibhujawa <vibhujawa@gmail.com>
2018-05-05 17:46:23 -0400

    pushed code

--- adaboost/adaboost.cpp
+++ adaboost/adaboost.cpp
@@ -20,54 +20,44 @@ vector<vector<double> > transpose(vector<vector<double> > ds)
 int main() {
     //cout << "Hello, World!" ;
 
-    int ds_len = 10;
-    int ds_feat = 2;
-    int t=2;
+    int ds_len = 5;
+    int ds_feat = 1;
+    int t=4;
 
-    vector<vector<double> > ds(ds_len, vector<double>(ds_feat, 0));
+    vector<vector<double> > X(ds_len, vector<double>(ds_feat, 0));
     vector<int> labels(ds_len,-1);
 
-    ds[0][0]=0.3; labels[0] = 1;
-    ds[1][0]=0.3; labels[1] = 1;
-    ds[2][0]=0.3; labels[2] = 1;
-    ds[3][0]=0.4; labels[3] = 1;
-    ds[4][0]=0.5; labels[4] = 1;
+    X[0][0]=0.3; labels[0] = 1;
+    X[1][0]=0.4; labels[1] = -1;
+    X[2][0]=0.5; labels[2] = -1;
+    X[3][0]=0.6; labels[3] = 1;
+    X[4][0]=0.7; labels[4] = 1;
 
-    ds[5][0]=1.3; labels[5] = 1;
-    ds[6][0]=1.4; labels[6] = -1;
-    ds[7][0]=1.5; labels[7] = -1;
-    ds[8][0]=1.6; labels[8] = -1;
-    ds[9][0]=1.7; labels[9] = -1;
+//    X[5][0]=1.3; labels[5] = -1;
+//    X[6][0]=1.4; labels[6] = -1;
+//    X[7][0]=1.5; labels[7] = -1;
+//    X[8][0]=1.6; labels[8] = 1;
+//    X[9][0]=1.7; labels[9] = 1;
 
 
-    for (int i = 0; i < ds.size(); ++i) {
-        for (int j = 0; j < ds[i].size(); ++j) {
-            cout << ds[i][j] << " ";
+    for (int i = 0; i < X.size(); ++i) {
+        for (int j = 0; j < X[i].size(); ++j) {
+            cout << X[i][j] << " ";
         }
         cout<<" Label "<<labels[i];
         cout<<"\n";
     }
-    vector<double>weights(ds_len,1.0/ds_len);
 
-    // Take the transpose of curr ds to make computation down the road easier
-
-    vector<vector<double> > ds_t = transpose(ds);
-
-    for(int i=0;i<t;++i) {
-        cout<<"\n Loop = "<<i<<"\n";
-
-        Feature_Stump best_feature_stump = get_best_feature_stump(labels, weights, ds_t);
-
-        cout << "\nBest Feature Index " << best_feature_stump.feature_index;
-        cout << "\nBest Feature Threshold " << best_feature_stump.threshold;
-        cout << "\nBest Feature Error " << best_feature_stump.error;
+    vector<vector<double> > ds_t = transpose(X);
+    Adaoost clf;
+    clf.fit(labels,ds_t,t);
+    vector<int> predictions = clf.predict(X);
+    cout<<"\n";
+    for(int i=0;i<predictions.size();++i)
+    {
+        cout<<"True Label "<<labels[i]<<" Predicted Label "<<predictions[i]<<"\n";
 
     }
-//    for(int i=0;i<weights.size();++i)
-//    {
-//        cout<<weights[i]<<"\n";
-//    }
-    cout<<"\n\n";
 
 
     return 0;
--- adaboost/adaboost.h
+++ adaboost/adaboost.h
@@ -7,7 +7,7 @@
 using namespace std;
 
 
-struct Feature_Stump
+struct Decision_Function
 {
     int feature_index = -1;
     double error = INT_MAX;
@@ -16,13 +16,20 @@ struct Feature_Stump
 
 };
 
-void update_weights(vector<int> &labels, double curr_error, vector<double> & weights,vector<double> & feature_vals,double threshold)
+struct Decision_Stump
+{
+    Decision_Function decision_function;
+    double alpha_t;
+
+};
+
+double update_weights(vector<int> &labels, double curr_error, vector<double> & weights,vector<double> & feature_vals,double threshold)
 {
     double alpha_t = 0.5 * log((1-curr_error)/(curr_error));
 
     double weights_sum = 0;
 
-    // cout<< "Alpha_t"<<alpha_t;
+    cout<< "\n Alpha_t "<<alpha_t<<"\n";
 
     for(int i = 0;i<weights.size();++i)
     {
@@ -40,7 +47,7 @@ void update_weights(vector<int> &labels, double curr_error, vector<double> & wei
     {
         weights[i]=weights[i]/weights_sum;
     }
-
+    return alpha_t;
 }
 
 
@@ -62,46 +69,106 @@ double get_error_curr_feature_val(vector<int> &labels,vector<double> & weights,v
 
 }
 
-Feature_Stump get_feature_threshold_curr_feature(vector<int> &labels, vector<double> &weights, vector<double> &feature_vals,vector<double> &feature_thresholds)
+Decision_Function get_feature_threshold_curr_feature(vector<int> &labels, vector<double> &weights, vector<double> &feature_vals,vector<double> &feature_thresholds)
 {
-    Feature_Stump best_feature_stump;
+    Decision_Function best_feature_threshold;
+
     for(int i=0;i<feature_thresholds.size();++i)
     {
         double curr_error = get_error_curr_feature_val(labels,weights,feature_vals,feature_thresholds[i]);
-        if (curr_error <= best_feature_stump.error)
+        if (curr_error <= best_feature_threshold.error)
         {
-            best_feature_stump.error = curr_error;
-            best_feature_stump.threshold=feature_thresholds[i];
-            best_feature_stump.feature_index = -1;
+            best_feature_threshold.error = curr_error;
+            best_feature_threshold.threshold=feature_thresholds[i];
+            best_feature_threshold.feature_index = -1;
 
         }
     }
-    return best_feature_stump;
+    return best_feature_threshold;
 }
 
-Feature_Stump get_best_feature_stump(vector<int> &labels, vector<double> &weights, vector<vector<double> > &feature_vals)
+Decision_Stump get_best_feature_stump(vector<int> &labels, vector<vector<double> > &feature_vals,vector<double> &weights)
 {
-    Feature_Stump best_feature_stump;
+    Decision_Stump best_feature_stump;
     for(int i=0;i<feature_vals.size();++i)
     {
-        Feature_Stump curr_feature_stump = get_feature_threshold_curr_feature(labels,weights,feature_vals[i],feature_vals[i]);
-        if (curr_feature_stump.error <= best_feature_stump.error)
+        Decision_Function curr_decision_function = get_feature_threshold_curr_feature(labels,weights,feature_vals[i],feature_vals[i]);
+        if (curr_decision_function.error < best_feature_stump.decision_function.error)
         {
-            best_feature_stump.error = curr_feature_stump.error;
-            best_feature_stump.threshold = curr_feature_stump.threshold;
-            best_feature_stump.feature_index = i;
-
-
+            best_feature_stump.decision_function.error = curr_decision_function.error;
+            best_feature_stump.decision_function.threshold = curr_decision_function.threshold;
+            best_feature_stump.decision_function.feature_index = i;
         }
     }
-    if (best_feature_stump.error >=0.0005)
-        update_weights(labels,best_feature_stump.error,weights,feature_vals[best_feature_stump.feature_index],best_feature_stump.threshold);
+    if (best_feature_stump.decision_function.error >=0.0005)
+        best_feature_stump.alpha_t = update_weights(labels,best_feature_stump.decision_function.error,weights,
+                       feature_vals[best_feature_stump.decision_function.feature_index],best_feature_stump.decision_function.threshold);
 
     return best_feature_stump;
 
 }
 
+vector<Decision_Stump> fit_weak_classifers(vector<int> &labels, vector<vector<double> > &ds_t,int t)
+{
 
+    vector<Decision_Stump> weak_classifiers;
+
+    vector<double>weights(labels.size(),1.0/sizeof(ds_t));
+    for(int i=0;i<t;++i) {
+        Decision_Stump curr_decision_stump = get_best_feature_stump(labels, ds_t,weights);
+        weak_classifiers.push_back(curr_decision_stump);
+        if (curr_decision_stump.decision_function.error <=0.00005)
+            return weak_classifiers;
+    }
+    return weak_classifiers;
+
+}
+
+
+class Adaoost{
+
+    vector<Decision_Stump> weak_classifiers;
+    public:
+
+        void fit(vector<int> &labels, vector<vector<double> > &ds_t,int t)
+        {
+            weak_classifiers = fit_weak_classifers(labels,ds_t,t);
+            for(int i =0;i<weak_classifiers.size();++i)
+            {
+                cout<<"Weak Classifier ["<<i<<"] index" << weak_classifiers[i].decision_function.feature_index<<" "\
+                     <<weak_classifiers[i].decision_function.threshold<<" ";
+                cout<<"Alpha_t "<<weak_classifiers[i].alpha_t<< "\n" ;
+            }
+        }
+        vector<int> predict(vector<vector<double> > &X) {
+            vector<int> pred_labels(X.size(),1);
+
+            for (int i = 0; i < X.size(); ++i) {
+                double negative_label_weight = 0;
+                double positive_label_weight = 0;
+                for (int j = 0; j < weak_classifiers.size(); ++j) {
+
+                    Decision_Stump curr_classifier = weak_classifiers[j];
+                    double  curr_feat_val = X[i][curr_classifier.decision_function.feature_index];
+                    if ( curr_feat_val <= curr_classifier.decision_function.threshold) {
+                        negative_label_weight += curr_classifier.alpha_t;
+                    } else {
+                        positive_label_weight += curr_classifier.alpha_t;
+                    }
+
+                }
+
+                if (negative_label_weight >= positive_label_weight) {
+                    pred_labels[i]=-1;
+                } else {
+                    pred_labels[i]=1;
+                }
+            }
+            return pred_labels;
+        }
+
+
+};
 
 
 #endif
