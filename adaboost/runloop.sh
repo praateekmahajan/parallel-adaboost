56b7a1a1ee311e6b260dabcb75d34145b6d78146
Praateek <praateekm@gmail.com>
2018-05-07 06:10:43 +0000

    runloop

--- adaboost/runloop.sh
+++ adaboost/runloop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 counter=1
-while [ $counter -le 20 ]
+while [ $counter -le 10 ]
 do
 	for (( k = 1; k < 33; k = k * 2 )); do
-		./a.out "$k" << output.txt
+		./a.out "$k" >> output.txt
 	done
 	counter=$[$counter+1]
 done
