c210ef3d1a092350270b3a259de454b026b2c393
vibhujawa <vibhujawa@gmail.com>
2018-05-08 00:11:51 -0400

    changed runloop

--- runloop.sh
+++ runloop.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 counter=1
-while [ $counter -le 2 ]
+while [ $counter -le 5 ]
 do
-	for (( k = 1; k < 33; k = k * 2 )); do
-		for (( i = 1000; i < 10000001 ; i = i * 10 )); do	
-			for (( j = 10; j < 10001; j = j * 10)); do
-				for (( h = 5; h < 161; h = h * 2)); do
-					#python create-dataset.py "$i" "$j"
-					#c++./a.out "$k" >> output.txt
-					c++./a.out "$k" "$h" "$i" "$j" >> output.txt
-				done
-			done
-		done
-	done
-	counter=$[$counter+1]
+        for (( h = 5; h < 41; h = h * 2)); do
+                for (( i = 1000; i < 10001 ; i = i * 10 )); do
+                        for (( j = 10; j < 1001; j = j * 10)); do
+                                for (( k = 1; k < 33; k = k * 2 )); do
+                                        ./a.out "$k" "$h" "$i" "$j" >> output.txt
+                                done
+                        done
+                done
+        done
+        counter=$[$counter+1]
 done
-
